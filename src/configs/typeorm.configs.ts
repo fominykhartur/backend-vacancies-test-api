b73aa321cfd0fdf2be22aa1013db65b8d40201d9
fominykhartur <fominykhartur+GitHub@gmail.com>
2023-09-12 22:38:39 +0300

    фикс

--- src/configs/typeorm.configs.ts
+++ src/configs/typeorm.configs.ts
@@ -7,6 +7,11 @@ import { Vacancies } from 'src/vacancies/vacancies.entity';
 export const getTypeormConfig = async (
   configService: ConfigService,
 ): Promise<TypeOrmModuleOptions> => {
+  console.log(configService.get('POSTGRES_HOST'));
+  console.log(configService.get('POSTGRES_PORT'));
+  console.log(configService.get('POSTGRES_USERNAME'));
+  console.log(configService.get('POSTGRES_PASSWORD'));
+  console.log(configService.get('POSTGRES_DB'));
   return {
     type: 'postgres',
     host: configService.get('POSTGRES_HOST'),

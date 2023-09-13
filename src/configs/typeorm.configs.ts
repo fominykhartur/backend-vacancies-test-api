da3bb88a065585a764b8de298d5e1ba40484f009
fominykhartur <fominykhartur+GitHub@gmail.com>
2023-09-13 15:47:27 +0300

    docker test

--- src/configs/typeorm.configs.ts
+++ src/configs/typeorm.configs.ts
@@ -14,7 +14,7 @@ export const getTypeormConfig = async (
   console.log(configService.get('POSTGRES_DB'));
   return {
     type: 'postgres',
-    host: configService.get('POSTGRES_HOST'),
+    host: process.env.POSTGRES_HOST, //configService.get('POSTGRES_HOST'),
     port: +configService.get('POSTGRES_PORT'),
     username: configService.get('POSTGRES_USERNAME'),
     password: configService.get('POSTGRES_PASSWORD'),
